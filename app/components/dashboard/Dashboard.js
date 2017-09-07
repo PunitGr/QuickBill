830af0f6d75fccae36ef7c7cec8781667f70643e
PunitGr <groove679@gmail.com>
2017-09-08 00:37:59 +0530

    Update design :fire:

--- app/components/dashboard/Dashboard.js
+++ app/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import SideNav from "./SideNav";
 import Invoice from "./Invoice";
+import { Link } from "react-router-dom";
 
 export default class Dashboard extends Component {
 
@@ -24,6 +25,15 @@ export default class Dashboard extends Component {
             <div className="dashboard">
                 <SideNav />
                 <Invoice />
+                <div className="dashboard__element">
+                    <div className="solid-btn solid-btn--ghost solid-btn--dashboard">
+                        <Link to="preview" className="ghost-btn"><i className="fa fa-eye" aria-hidden="true"> </i> Preview</Link>
+                        <a className="ghost-btn"><i className="fa fa-arrow-circle-down" aria-hidden="true"> </i> Download</a>
+                    </div>
+                    <a className="solid-btn solid-btn--rect solid-btn--dashboard">
+                        <i className="fa fa-paper-plane" aria-hidden="true"> </i> Send Invoice
+                    </a>
+                </div>
             </div>
         );
     }
