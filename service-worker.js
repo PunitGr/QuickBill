3aa039fed56ee323745f4a2529a21b63657bdc79
PunitGr <groove679@gmail.com>
2017-09-13 03:13:21 +0530

    Minor changes

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,35 @@
-let doCache = false;
-
-let CACHE_NAME = 'quickbill-cache-v1';
+let CACHE_NAME = 'QuickBill';
 
 let urlsToCache = [
-  '/',
-  './assets/css/styles.css',
-  './assets/images/*',
-  './dist/index-bundle.js'
+    'assets/css/styles.css',
+    'assets/images/*',
+    'dist/index-bundle.js'
 ];
 
-self.addEventListener("activate", event => {
-    const cacheWhiteList = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys()
-            .then(keyList => 
-                Promise.all(keyList.map(key => {
-                    if (!cacheWhiteList.includes(key)) {
-                        console.log('Deleting Cache: ' + key);
-                        return caches.delete(key);
-                    }
-                }))
-            )
-    );
+self.addEventListener('activate',  event => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("install", function(event) {
-    if (doCache) {
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                    fetch("manifest.json")
-                        .then(resp => {
-                            resp.json();
-                        })
-                        .then(() => {
-                            cache.addAll(urlsToCache);
-                            console.log('cached');
-                        })
-                })
-        )
-    }
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                fetch("manifest.json")
+                    .then(resp => {
+                        resp.json();
+                    })
+                    .then(() => {
+                        cache.addAll(urlsToCache);
+                        console.log('cached');
+                    })
+            })
+    )
 })
 
 self.addEventListener('fetch', function(event) {
-    if (doCache) {
-      event.respondWith(
-          caches.match(event.request).then(function(response) {
-              return response || fetch(event.request);
-          })
-      );
-    }
+    event.respondWith(
+        caches.match(event.request, {ignoreSearch:true}).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
